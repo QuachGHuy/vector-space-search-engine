58c5b155963ec7d2d43ea87e7dfe4a880bed400c
QuachGHuy <quachhuy97@gmail.com>
2026-03-17 10:18:37 +0700

    feat(middleware): add structured logging middleware

--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
 import uvicorn
 from fastapi import FastAPI
+
 from src.api.lifespan import lifespan
+from core.middlewares import StructuredLoggingMiddleware
 from src.api.routers.search import router as search_router
+
 
 app = FastAPI(
     title="Vector Space Search Engine",
     lifespan=lifespan
 )
+
+app.add_middleware(StructureLoggingMiddleware)
 
 app.include_router(search_router)
 
